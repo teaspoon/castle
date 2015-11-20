465cbdf50d8a383103386a0ed2b3cd98bea7031b
teaspoon <kwaad.lepel@gmail.com>
2015-11-20 17:12:13 +0200

    begin to include ports and signals

--- web/elm/Castle.elm
+++ web/elm/Castle.elm
@@ -3,15 +3,25 @@ module Castle where
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-import StartApp.Simple
+import StartApp
+import Effects exposing (Effects, Never)
+import Task exposing (Task)
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
 
 main : Signal Html
 main =
-  StartApp.Simple.start
-    { model = init
-    , update = update
-    , view = view
-    }
+  app.html
+
+port tasks : Signal (Task Never())
+port tasks =
+  app.tasks
 
 -- MODEL
 
@@ -28,19 +38,23 @@ type alias Book =
 type alias Model =
   List Book
 
-init : Model
+init : (Model, Effects Action)
 init =
-  [ {isbn = "978-0575097360", title = "The Way of Kings Part One", genre = "Fantasy", author = "Brandon Sanderson", word_count = 200000, pages = 608, status = "Currently Reading"}
-  , {isbn = "978-0575102484", title = "The Way of Kings Part Two", genre = "Fantasy", author = "Brandon Sanderson", word_count = 187000, pages = 585, status = "Up Next"}
-  , {isbn = "978-0575081406", title = "The Name of the Wind (The Kingkiller Chronicle)", genre = "Fantasy", author = "Patrick Rothfuss", word_count = 259000, pages = 672, status = "Completed"}
-  ]
+  let
+    books =
+      [ {isbn = "978-0575097360", title = "The Way of Kings Part One", genre = "Fantasy", author = "Brandon Sanderson", word_count = 200000, pages = 608, status = "Currently Reading"}
+      , {isbn = "978-0575102484", title = "The Way of Kings Part Two", genre = "Fantasy", author = "Brandon Sanderson", word_count = 187000, pages = 585, status = "Up Next"}
+      , {isbn = "978-0575081406", title = "The Name of the Wind (The Kingkiller Chronicle)", genre = "Fantasy", author = "Patrick Rothfuss", word_count = 259000, pages = 672, status = "Completed"}
+      ]
+  in
+    (books, Effects.none)
 
 
 -- UPDATE
 
 type Action = Toggle Book
 
-update : Action -> Model -> Model
+update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     Toggle bookToToggle ->
@@ -53,7 +67,7 @@ update action model =
               { bookFromModel | status = "Completed" }
           else bookFromModel
       in
-        List.map updateBook model
+        (List.map updateBook model, Effects.none)
 
 
 -- VIEW
