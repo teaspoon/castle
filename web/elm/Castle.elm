2fab1b2156f56898794b523282771d5987983ee2
teaspoon <kwaad.lepel@gmail.com>
2015-11-20 15:16:43 +0200

    adding the book model

--- web/elm/Castle.elm
+++ web/elm/Castle.elm
@@ -1,11 +1,36 @@
 module Castle where
 
-import Html
+import Html exposing (ul, li, text)
+import Html.Attributes exposing (class)
 
 main =
-  view
+  view init
+
+-- MODEL
+
+type alias Book =
+  { isbn : String
+  , title : String
+  , genre : String
+  , author : String
+  , word_count : Int
+  , pages : Int
+  , status : String
+  }
+
+type alias Model =
+  List Book
+
+init =
+  [ {isbn = "978-0575097360", title = "The Way of Kings Part One", genre = "Fantasy", author = "Brandon Sanderson", word_count = 200000, pages = 608, status = "Currently Reading"}
+  , {isbn = "978-0575102484", title = "The Way of Kings Part Two", genre = "Fantasy", author = "Brandon Sanderson", word_count = 187000, pages = 585, status = "Up Next"}
+  , {isbn = "978-0575081406", title = "The Name of the Wind (The Kingkiller Chronicle)", genre = "Fantasy", author = "Patrick Rothfuss", word_count = 259000, pages = 672, status = "Completed"}
+  ]
 
 -- VIEW
 
-view =
-  Html.text "Hello from elm"
+view model =
+  ul [ class "books" ] (List.map bookItem model)
+
+bookItem book =
+  li [ class "book" ] [ text book.title ]
